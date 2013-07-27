e0a89538182f4a9c2c3177c5f18d78c3181c34c6
Saketh Kasibatla <sake.kasi@gmail.com>
2013-07-27 03:00:01 -0700

    added time/get most recent to library

--- wimedb.py
+++ wimedb.py
@@ -1,11 +1,12 @@
 import sqlite3
 import eyeD3
+import time
 
 def insert(filename, database):
     conn = sqlite3.connect(database)
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS queue
-                 (FILE text, SONG text, ARTIST text)''')
+                 (FILE text, SONG text, ARTIST text, TIME bigint)''')
 
     song_data = []
     song_data.append(filename)
@@ -16,8 +17,9 @@ def insert(filename, database):
     
     song_data.append(tag.getTitle())
     song_data.append(tag.getArtist())
+    song_data.append(int(round(time.time() * 1000)))
 
-    c.execute("INSERT INTO queue VALUES (?,?,?)", song_data)
+    c.execute("INSERT INTO queue VALUES (?,?,?,?)", song_data)
 
     conn.commit()
     conn.close()
@@ -28,6 +30,18 @@ def get(filename, database):
     c.execute("SELECT * FROM queue WHERE FILE = ?", [filename] )
 
     ret = c.fetchone()
+    conn.close()
+    return ret
+
+def get_most_recent(database):
+    conn = sqlite3.connect(database)
+    c = conn.cursor()
+    c.execute("SELECT MIN(TIME) FROM queue")
+    time = c.fetchone()[0]
+
+    c.execute("SELECT * FROM queue WHERE TIME = ?", [time])
+    ret = c.fetchone()
+
     conn.close()
     return ret
 
